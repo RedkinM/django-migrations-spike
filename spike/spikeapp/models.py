2ca033dddd6d1a6321a3d37f7618f04dab580605
Michail Redkin <m.redkin@dodopizza.com>
2017-07-13 14:57:50 +0300

    migrations

--- spike/spikeapp/models.py
+++ spike/spikeapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default="")
+    last_name = models.CharField(max_length=255, default="")
